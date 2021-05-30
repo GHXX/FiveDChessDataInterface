e289f770348eeb0d57d11973bc5476192d6b15f2
CrazyPenguin0111 <85075129+CrazyPenguin0111@users.noreply.github.com>
2021-05-30 15:24:21 -0400

    Add files via upload
    Fixed Board Size Issues

---
+++ DataInterfaceConsoleTest/Examples/FeatureToggleSnippetss.cs
@@ -0,0 +1,190 @@
+﻿using FiveDChessDataInterface;
+using System;
+using System.Linq;
+using System.Reflection;
+using static DataInterfaceConsoleTest.Examples.CallableExMethodAttribute;
+
+namespace DataInterfaceConsoleTest.Examples
+{
+    static class featureToggleSnippetss
+    {
+        private const bool ENABLE_SNIPPETS = true; //enables use of snippets, the individual snippet must also be set to true
+        public static MethodInfo[] GetEnabledMethods() => !ENABLE_SNIPPETS ? new MethodInfo[] { } :
+            typeof(featureToggleSnippetss).GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
+            .Where(x => x.GetCustomAttribute<CallableExMethodAttribute>()?.Enabled == true).ToArray();
+
+        [CallableExMethod(false, InvokeKind.MatchStart)]
+        public static void DuplicateTimelinesMsg(DataInterface di)
+        {
+            Console.WriteLine("Timeline Duplicated or the console crashed");
+        }
+
+        [CallableExMethod(true, InvokeKind.MatchStart)]
+        public static void ChangeBoardSize(DataInterface di) //Changes board size for all boards
+        {
+            di.MemLocChessBoardSizeHeight.SetValue(7);
+            di.MemLocChessBoardSizeWidth.SetValue(7);
+            Console.WriteLine("Board Sized has Changed");
+        }
+
+        [CallableExMethod(false, InvokeKind.TurnChange)]
+        public static void OnTurnChangedMsg(DataInterface di)
+        {
+            Console.WriteLine($"The turn changed! Currently it is {(di.GetCurrentPlayersTurn() == 0 ? "WHITE" : "BLACK")}'s turn.");
+        }
+
+        [CallableExMethod(false, InvokeKind.BoardCountChanged | InvokeKind.MatchStart)]
+        public static void UpgradePawnsToQueensAfterTurn3(DataInterface di)
+        {
+            di.ModifyChessBoards(cb =>
+            {
+                if (cb.cbm.moveType == 0 && // no move has been made on this board yet
+                        cb.cbm.turn >= 3) // if its turn 4 or later
+                {
+                    cb.Pieces = cb.Pieces
+                    .Select(x => new ChessBoard.ChessPiece((x.Kind == ChessBoard.ChessPiece.PieceKind.Pawn) ? ChessBoard.ChessPiece.PieceKind.Queen : x.Kind, x.IsBlack))
+                    .ToArray(); //Changes Pawns to Queens
+                }
+
+                return cb;
+            });
+        }
+        
+        
+        [CallableExMethod(false, InvokeKind.MatchStart)]
+        public static void PrependTurnZero(DataInterface di)
+        {
+            var baseBoards = di.GetChessBoards();
+
+            if (baseBoards.First(x => x.cbm.boardId == 0).cbm.isBlacksMove != 0)
+                return; // exit if this is a turnzero game
+
+            var timelines = baseBoards.Select(x => x.cbm).GroupBy(x => x.timeline).ToList();
+
+            int boardId = 0;
+            var newBoards = timelines.SelectMany(timeLineBoards =>
+            {
+                var tlBoards = timeLineBoards.Prepend(timeLineBoards.First()).ToList();
+
+                for (int boardIndex = 0; boardIndex < tlBoards.Count; boardIndex++)
+                {
+                    var cbm = tlBoards[boardIndex];
+                    if (boardIndex == 0)
+                    {
+                        cbm.isBlacksMove = 1;
+                        cbm.turn = 0;
+                        cbm.moveTurn = 0;
+                        cbm.moveType = 5;
+                    }
+                    else
+                        cbm.turn++;
+
+                    tlBoards[boardIndex] = cbm;
+                }
+                return tlBoards;
+            })
+                .OrderBy(x => x.turn)
+                .ThenBy(x => x.timeline * x.timeline)
+                .Select(x =>
+                {
+                    x.boardId = boardId++;
+                    return x;
+                })
+                .GroupBy(x => x.timeline)
+                .SelectMany(group =>
+                {
+                    var boards = group.ToArray();
+                    for (int i = 1; i < boards.Length; i++)
+                    {
+                        boards[i].previousBoardId = boards[i - 1].boardId;
+                        boards[i].creatingMoveNumber = boards[i - 1].boardId;
+                        boards[i - 1].nextInTimelineBoardId = boards[i].boardId;
+                    }
+
+                    return boards;
+                })
+                .Select(x => new ChessBoard(x, baseBoards[0].width, baseBoards[0].height)).ToArray();
+
+            di.SetChessBoardArray(newBoards.ToArray());
+            di.MemLocCosmeticTurnOffset.SetValue(-1);
+            di.RecalculateBitboards();
+        }
+
+        [CallableExMethod(false, InvokeKind.MatchStart)]
+        public static void AddNewTimelines(DataInterface di)
+        {
+            var baseBoards = di.GetChessBoards();
+            int dimcnt = baseBoards.Select(x => x.cbm.timeline).Distinct().Count() + 2;
+            int boardId = 0;
+            var boards = Enumerable.Range(-dimcnt / 2, dimcnt).SelectMany(timeline =>
+                  baseBoards.Select(baseBoard =>
+                  {
+                      var cbm = baseBoard.cbm;
+                      cbm.timeline = timeline;
+                      return cbm;
+                  })
+                )
+                .OrderBy(x => x.turn)
+                .ThenBy(x => x.timeline * x.timeline)
+                .Select(x =>
+                {
+                    x.boardId = boardId++;
+                    return x;
+                })
+                .GroupBy(x => x.timeline)
+                .SelectMany(group =>
+                {
+                    var boards = group.ToArray();
+                    for (int i = 1; i < boards.Length; i++)
+                    {
+                        boards[i].previousBoardId = boards[i - 1].boardId;
+                    }
+
+                    return boards;
+                })
+                .OrderBy(x => x.boardId)
+                .Select(x => new ChessBoard(x, baseBoards[0].width, baseBoards[0].height)).ToArray();
+
+            di.SetChessBoardArray(boards.ToArray());
+            di.RecalculateBitboards();
+        }
+
+        [CallableExMethod(true, InvokeKind.BoardCountChanged | InvokeKind.Startup | InvokeKind.MatchStart | InvokeKind.MatchExited)]
+        public static void DumpBoardsAndGeneralInfo(DataInterface di)
+        {
+            var cbs = di.GetChessBoards();
+
+            Console.Clear();
+            Console.WriteLine($"Current chessboard ptr: {di.MemLocChessArrayPointer.ToString()}");
+            Console.WriteLine($"Current timeline stats: White: {di.GetNumberOfWhiteTimelines()}; Black: {di.GetNumberOfBlackTimelines()}");
+            Console.WriteLine("Chessboards: \n");
+            for (int i = 0; i < cbs.Count; i++)
+            {
+                var board = cbs[i];
+                Console.WriteLine($"Board: L{board.cbm.timeline:+#;-#;0}T{board.cbm.turn + 1}");
+
+
+
+                for (int y = board.height - 1; y >= 0; y--)
+                {
+                    for (int x = 0; x < board.width; x++)
+                    {
+                        var p = board.Pieces[x * board.width + y];
+
+                        if (!p.IsEmpty)
+                        {
+                            Program.WriteConsoleColored(p.SingleLetterNotation(), p.IsBlack ? ConsoleColor.White : ConsoleColor.Black, p.IsBlack ? ConsoleColor.Black : ConsoleColor.White);
+                        }
+                        else
+                        {
+                            Program.WriteConsoleColored(" ", ConsoleColor.Gray, ConsoleColor.Gray);
+                        }
+                    }
+                    Console.ResetColor();
+                    Console.WriteLine(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
