be107c0a69bbcb407ed354969c54c88638e8a883
GHXX <GHXX@gmx.at>
2021-06-02 19:30:04 +0200

    improved the AddNewTimelines snippet

--- DataInterfaceConsoleTest/Examples/ExampleSnippets.cs
+++ DataInterfaceConsoleTest/Examples/ExampleSnippets.cs
@@ -1,5 +1,7 @@
 ﻿using FiveDChessDataInterface;
+using FiveDChessDataInterface.Util;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using static DataInterfaceConsoleTest.Examples.CallableExMethodAttribute;
@@ -117,37 +119,156 @@ namespace DataInterfaceConsoleTest.Examples
         [CallableExMethod(true, InvokeKind.MatchStart)]
         public static void AddNewTimelines(DataInterface di)
         {
-            var baseBoards = di.GetChessBoards();
-            int dimcnt = baseBoards.Select(x => x.cbm.timeline).Distinct().Count() + 30;
-            int boardId = 0;
-            var boards = Enumerable.Range(-dimcnt / 2, dimcnt).SelectMany(timeline =>
-                  baseBoards.Select(baseBoard =>
-                  {
-                      var cbm = baseBoard.cbm;
-                      cbm.timeline = timeline;
-                      return cbm;
-                  })
-                )
-                .OrderBy(x => x.turn)
-                .ThenBy(x => Math.Abs(x.timeline))
-                .Select(x =>
-                {
-                    x.boardId = boardId++;
-                    return x;
-                })
-                .GroupBy(x => x.timeline)
-                .SelectMany(group =>
-                {
-                    var boards = group.ToArray();
-                    for (int i = 1; i < boards.Length; i++)
-                    {
-                        boards[i].previousBoardId = boards[i - 1].boardId;
-                    }
 
-                    return boards;
-                })
-                .OrderBy(x => x.boardId)
-                .Select(x => new ChessBoard(x, baseBoards[0].width, baseBoards[0].height)).ToArray();
+            // adds the following amount of timelines for both black and white each.
+            // e.g. a value of 1 would add one timeline on the bottom and one at the top
+            int timelinesToAddForEachPlayer = 1;
+
+
+            var baseBoards = di.GetChessBoards();
+
+            var baseCbms = baseBoards.Select(x => x.cbm).ToList();
+            int newId = baseBoards.Max(x => x.cbm.boardId) + 1;
+            for (int _ = 0; _ < timelinesToAddForEachPlayer; _++)
+            {
+                var newBoards = baseCbms;
+
+                // add new black timeline
+                var minTL = baseCbms.Min(x => x.timeline);
+                var newMinTLBoards = baseCbms.Where(x => x.timeline == minTL)
+                    .Select(x =>
+                    {
+                        var newCbm = x;
+                        newCbm.timeline--;
+                        newCbm.boardId = newId++;
+                        return newCbm;
+                    })
+                    .OrderBy(x => x.GetSubturnIndex()).ToList();
+
+
+                // add new white timeline
+                var maxTL = baseCbms.Max(x => x.timeline);
+                var newMaxTLBoards = baseCbms.Where(x => x.timeline == maxTL)
+                    .Select(x =>
+                    {
+                        var newCbm = x;
+                        newCbm.timeline++;
+                        newCbm.boardId = newId++;
+                        return newCbm;
+                    })
+                    .OrderBy(x => x.GetSubturnIndex()).ToList();
+
+                IEnumerable<int> RangeFromToInclusive(int start, int end)
+                {
+                    return Enumerable.Range(start, end - start + 1);
+                }
+
+                // combine new boards 
+                var boardsToInsert = newMinTLBoards.Concat(newMaxTLBoards).ToList();
+                newBoards.AddRange(boardsToInsert);
+
+                baseCbms = newBoards;
+            }
+
+            var sortedBoards = GameUtil.ReassignBoardIds(baseCbms.ToArray());
+
+
+            //int timelinesToAdd = timelinesToAddForEachPlayer * 2;
+            //var baseBoards = di.GetChessBoards();
+            //var baseCbms = baseBoards.Select(x => x.cbm).ToList();
+
+            //int startingTimelineCount = baseCbms.DistinctBy(x => x.timeline).Count();
+            //var originalBoardsByTurn = baseCbms.GroupBy(x => x.timeline).ToList();
+
+            //var newBoards = new List<ChessBoardMemory>();
+            //var subturns = baseCbms.Select(x => x.GetSubturnIndex()).Distinct();
+            //int shiftAmount = 0;
+            //for (int subturn = subturns.Min(); subturn <= subturns.Max(); subturn++) // iterate all turns
+            //{
+            //    var currTurnBoards = baseCbms.Where(x => x.GetSubturnIndex() == subturn).ToList();
+            //    var highestBoardIndexInTurn = currTurnBoards.Max(x => x.boardId);
+            //    // shift boards by the number of ids we added
+
+            //    var sortedByTimeline = currTurnBoards.OrderBy(x => x.timeline).ToList();
+
+            //    var minLBoard = sortedByTimeline.First(); // black-most timeline
+            //    //bool allowAddingBlackTimelineBoards = ;
+            //    var maxLBoard = sortedByTimeline.Last(); // white-most timeline
+            //    //var originalMaxTimeline = maxLBoard.timeline;
+
+            //    var shiftedNewBoards = new List<ChessBoardMemory>();
+            //    foreach (var cbm in sortedByTimeline)
+            //    {
+            //        var shiftedCbm = cbm;
+            //        shiftedCbm.boardId += shiftAmount;
+            //        shiftedNewBoards.Add(shiftedCbm);
+            //    }
+
+            //    int GetNextId()
+            //    {
+            //        var newId = shiftedNewBoards.Max(x => x.boardId) + 1;
+            //        if (newBoards.Any(x => x.boardId == newId))
+            //            throw new Exception("A boardId has been assigned twice. This indicates a problem in this algorithm.");
+
+            //        return newId;
+            //    }
+
+            //    for (int l = 0; l < timelinesToAddForEachPlayer; l++)
+            //    {
+            //        if (baseCbms.Any(x => x.timeline == minLBoard.timeline))
+            //        {
+            //            minLBoard.timeline--;
+            //            minLBoard.boardId = GetNextId();
+            //            shiftedNewBoards.Add(minLBoard);
+
+            //            shiftAmount++;
+            //        }
+
+            //        if (baseCbms.Any(x => x.timeline == maxLBoard.timeline))
+            //        {
+            //            maxLBoard.timeline++;
+            //            maxLBoard.boardId = GetNextId();
+            //            shiftedNewBoards.Add(maxLBoard);
+            //            shiftAmount++;
+            //        }
+            //    }
+
+            //    newBoards.AddRange(shiftedNewBoards);
+            //}
+
+
+            //int dimcnt = baseBoards.Select(x => x.cbm.timeline).Distinct().Count() + 2;
+            //int boardId = 0;
+            //var boards = Enumerable.Range(-dimcnt / 2, dimcnt).SelectMany(timeline =>
+            //      baseBoards.Select(baseBoard =>
+            //      {
+            //          var cbm = baseBoard.cbm;
+            //          cbm.timeline = timeline;
+            //          return cbm;
+            //      })
+            //    )
+            //    .OrderBy(x => x.turn)
+            //    .ThenBy(x => Math.Abs(x.timeline))
+            //    .Select(x =>
+            //    {
+            //        x.boardId = boardId++;
+            //        return x;
+            //    })
+            //    .GroupBy(x => x.timeline)
+            //    .SelectMany(group =>
+            //    {
+            //        var boards = group.ToArray();
+            //        for (int i = 1; i < boards.Length; i++)
+            //        {
+            //            boards[i].previousBoardId = boards[i - 1].boardId;
+            //        }
+
+            //        return boards;
+            //    })
+            //    .OrderBy(x => x.boardId)
+            //    .Select(x => new ChessBoard(x, baseBoards[0].width, baseBoards[0].height)).ToArray();
+
+            var boards = sortedBoards.OrderBy(x => x.boardId).Select(x => new ChessBoard(x, baseBoards[0].width, baseBoards[0].height)).ToArray();
 
             di.SetChessBoardArray(boards.ToArray());
             di.RecalculateBitboards();
