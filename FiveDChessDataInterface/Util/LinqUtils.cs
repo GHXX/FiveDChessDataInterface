e8be9f26adca364192e9b97a4b3b0487e5884115
GHXX <GHXX@gmx.at>
2021-06-02 19:27:57 +0200

    added DistinctBy utility function

---
+++ FiveDChessDataInterface/Util/LinqUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FiveDChessDataInterface.Util
+{
+    internal static class LinqUtils
+    {
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
+    }
+}
