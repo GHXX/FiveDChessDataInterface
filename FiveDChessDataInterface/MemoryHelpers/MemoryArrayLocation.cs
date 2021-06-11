200368e9f719338908b49582627a7c825feb349a
GHXX <GHXX@gmx.at>
2021-06-11 21:34:10 +0200

    implemented memoryarraylocation

--- FiveDChessDataInterface/MemoryHelpers/MemoryArrayLocation.cs
+++ FiveDChessDataInterface/MemoryHelpers/MemoryArrayLocation.cs
@@ -1,25 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Runtime.InteropServices;
 
 namespace FiveDChessDataInterface.MemoryHelpers
 {
-    class MemoryArrayLocation<ElementType>
+    class MemoryArrayLocation<ElementType> where ElementType : unmanaged
     {
         private readonly MemoryLocation<IntPtr> memLocBaseArrayPtr;
         private readonly MemoryLocation<int> memLocCapacity;
         private readonly MemoryLocation<int> memLocElementCount;
+
+        private readonly int elementSize;
 
         public MemoryArrayLocation(MemoryLocation<IntPtr> baseArrayPtr)
         {
             this.memLocBaseArrayPtr = baseArrayPtr;
             this.memLocCapacity = baseArrayPtr.WithOffset<int>(-0x4);
             this.memLocElementCount = baseArrayPtr.WithOffset<int>(-0x8);
+
+            this.elementSize = Marshal.SizeOf(typeof(ElementType));
         }
 
-        public ElementType[] GetValues()
+        public ElementType[] GetArray()
         {
-            return null;
+            var cnt = this.memLocElementCount.GetValue();
+            var cap = this.memLocCapacity.GetValue();
+
+            if (cnt > cap)
+                throw new InvalidOperationException("Array size was larger than the array capacity!");
+
+            var arr = new ElementType[cnt];
+            var basePtr = this.memLocBaseArrayPtr.GetValue();
+            for (int i = 0; i < cnt; i++)
+            {
+                arr[i] = (ElementType)Marshal.PtrToStructure(IntPtr.Add(basePtr, i * this.elementSize), typeof(ElementType));
+            }
+
+            return arr;
         }
+
+        public ElementType this[int index]
+        {
+            get
+            {
+                return GetArray()[index]; // TODO optimize            
+            }
+            set
+            {
+                if (index < 0 || index > this.memLocCapacity.GetValue())
+                    throw new IndexOutOfRangeException($"Array is too small for the specified index {index}");
+
+                var elementPtr = IntPtr.Add(this.memLocBaseArrayPtr.GetValue(), index * this.elementSize);
+                Marshal.StructureToPtr(value, elementPtr, false);
+            }
+        }
+
     }
 }
