b5f1846bc98bb0d4a77e5b960f2a54dbba4b8dc7
GHXX <GHXX@gmx.at>
2021-06-18 11:24:36 +0200

    added frontend console project, designated for non-developer-use

---
+++ DataInterfaceConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace DataInterfaceConsole
+{
+    class Program
+    {
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+            Console.Title = "5D Data Interface Console";
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
