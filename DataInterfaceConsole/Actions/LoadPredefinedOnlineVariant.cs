72143633456cd054cff1f26b6aa632063f2bdd3c
GHXX <GHXX@gmx.at>
2021-06-27 12:47:20 +0200

    implemented trapping for the online and offline variants

---
+++ DataInterfaceConsole/Actions/LoadPredefinedOnlineVariant.cs
@@ -0,0 +1,14 @@
+﻿using FiveDChessDataInterface.MemoryHelpers;
+using FiveDChessDataInterface.Variants;
+using System;
+using System.Threading;
+
+namespace DataInterfaceConsole.Actions
+{
+    class LoadPredefinedOnlineVariant : BaseLoadPredefinedVariant
+    {
+        public override string Name => "Load Predefined Variant [ONLINE]";
+
+        protected override bool UseOnlineVariants => true;
+    }
+}
