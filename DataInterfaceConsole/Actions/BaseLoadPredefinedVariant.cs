ff296c011d03cba8be7f2aad2ee28a3c8f7494b1
GHXX <GHXX@gmx.at>
2021-08-16 09:39:27 +0200

    added helper message

--- DataInterfaceConsole/Actions/BaseLoadPredefinedVariant.cs
+++ DataInterfaceConsole/Actions/BaseLoadPredefinedVariant.cs
@@ -34,9 +34,10 @@ namespace DataInterfaceConsole.Actions
                 var variants = this.UseOnlineVariants ? GithubVariantGetter.GetAllVariants(false, false) : GithubVariantGetter.GetAllVariants(true, true);
 
                 WriteLineIndented("Select a variant from the following:" + (at == null ? " (Variants that would may lead to a crash in case they are loaded right now are indicated in yellow)" : ""));
+                bool anyCrashed = false;
+                var originalConsoleColor = Console.ForegroundColor;
                 for (int i = 0; i < variants.Length; i++)
                 {
-                    var originalConsoleColor = Console.ForegroundColor;
                     if (at == null) // if we are using direct injection, build the variant and check how many boards it has.
                     {
                         try
@@ -50,13 +51,21 @@ namespace DataInterfaceConsole.Actions
                         catch
                         {
                             Console.ForegroundColor = ConsoleColor.Red;
+                            anyCrashed = true;
                         }
                     }
-
                     WriteLineIndented($"{(i + 1).ToString().PadLeft((int)Math.Ceiling(Math.Log10(variants.Length)))}. {variants[i].Name} by {variants[i].Author}");
 
                     Console.ForegroundColor = originalConsoleColor;
                 }
+
+
+                if (anyCrashed)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    WriteLineIndented(($"It appears that one or more variants cannot be loaded. Attempting to do so will lead to a crash. These are marked in red.\n" +
+                                           "If you have not made these variants yourself, consider getting the latest release from github (https://5dr.ghxx.dev).").Split('\n'));                }
+                Console.ForegroundColor = originalConsoleColor;
 
                 if (int.TryParse(Util.ConsoleReadLineWhile(() => this.di.IsValid()), out int input) && input > 0 && input <= variants.Length)
                 {
