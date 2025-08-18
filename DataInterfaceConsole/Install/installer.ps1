25e09b9c3662182f15d0ed9c00e9528edf36db9c
GHXX <GHXX@gmx.at>
2025-08-18 16:24:17 +0200

    update to .net8

--- DataInterfaceConsole/Install/installer.ps1
+++ DataInterfaceConsole/Install/installer.ps1
@@ -1,17 +1,17 @@
 $ErrorActionPreference = 'SilentlyContinue'
-dotnet --list-runtimes | findstr /C:"Microsoft.NETCore.App 3.1"
+dotnet --list-runtimes | findstr /C:"Microsoft.NETCore.App 8.0"
 $ErrorActionPreference = 'Continue'
 
 If ($lastExitCode -eq "0") {
-    Write-Host ".NETCore 3.1 is already installed."
+    Write-Host ".NETCore 8.0 is already installed."
 }
 else
 {
     $confirmation = Read-Host ".NETCore 3.1 appears to be not installed yet. Install it now? [y/n]"
 	if ($confirmation -eq 'y') {
 		Write-Host "Running installer..."
-		$dlUri = "https://download.visualstudio.microsoft.com/download/pr/7cea63ad-1e76-41f0-a54a-eacb48fec749/87c339835cd7647c0fee3f14820cd909/windowsdesktop-runtime-3.1.16-win-x64.exe"
-		$outpath = "dotnetinstaller31.exe"
+		$dlUri = "https://builds.dotnet.microsoft.com/dotnet/Runtime/8.0.19/dotnet-runtime-8.0.19-win-x64.exe"
+		$outpath = "dotnetinstaller80.exe"
 		Invoke-Webrequest -Uri $dlUri -OutFile $outpath
 		&cmd /c "$outpath /install /quiet /norestart"
 	}
--- DataInterfaceConsole/Types/Exceptions/DataInterfaceClosedException.cs
+++ DataInterfaceConsole/Types/Exceptions/DataInterfaceClosedException.cs
@@ -9,8 +9,5 @@ namespace DataInterfaceConsole.Types.Exceptions
         public DataInterfaceClosedException() { }
         public DataInterfaceClosedException(string message) : base(message) { }
         public DataInterfaceClosedException(string message, Exception inner) : base(message, inner) { }
-        protected DataInterfaceClosedException(
-          System.Runtime.Serialization.SerializationInfo info,
-          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
 }
